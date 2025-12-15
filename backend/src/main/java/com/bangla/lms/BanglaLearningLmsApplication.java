8b6f100c7bbc08e28c15469ae5726c69aa10272b
noor52 <nooralam52cse@gmail.com>
2025-12-16 00:27:16 +0600

    c

--- backend/src/main/java/com/bangla/lms/BanglaLearningLmsApplication.java
+++ backend/src/main/java/com/bangla/lms/BanglaLearningLmsApplication.java
@@ -2,8 +2,12 @@ package com.bangla.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.bangla.lms.repository")
+@EntityScan(basePackages = "com.bangla.lms.entity")
 public class BanglaLearningLmsApplication {
 
     public static void main(String[] args) {
